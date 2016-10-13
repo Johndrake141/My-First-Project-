a354d5d1c2bc2182fd4a29f947cccec13b1a4e25
Johndrake141 <77429@bwls.net>
2016-10-13 15:09:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
-    createCanvas( 800 , 800 ); fill( r )
+    createCanvas( 800 , 800 ); 
 }
 
 function draw() {
-    ellipse( mouseX , mouseY ,110 , 110 ); 
+    ellipse( mouseX , mouseY ,110 , 110 ); fill(0,255,0)
 }
